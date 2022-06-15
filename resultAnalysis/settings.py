33ecbcb699d029568315f87a30f5725b75f4f005
Devan <devankhandagale@gmail.com>
2022-06-15 15:18:08 +0530

    staticfiles

--- resultAnalysis/settings.py
+++ resultAnalysis/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
